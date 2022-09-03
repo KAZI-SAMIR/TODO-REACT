1d09cd4dc8465af986142c2fcab57b1491c464d9
KAZI-SAMIR <kazi46616@gmail.com>
2022-09-03 21:01:34 +0600

    todo done

---
+++ src/app.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Form from "./components/form";
+import Footer from './components/footer';
+
+function App() {
+  return (
+    
+    <div>
+        <div className="todo-table">
+        <div><center><h2> Kz todolist </h2></center></div>
+      <Form />
+    </div>
+    <Footer/>
+    </div>
+    
+    
+  );
+}
+
+export default App;
---
+++ src/components/footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer">
+        Made by{" "}
+        <a rel="noopener noreferrer" target="_blank" href="https://github.com/KAZI-SAMIR">
+          kaziSamir{" "}
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
